1008bcd2cbc9105dd0d20451d10b503bc42cabe8
Noah Park <noahjinpark@gmail.com>
2021-07-19 16:16:12 -0500

    Made more visual changes and added Bubble sort.

--- src/Algorithms/SortingAlgorithms.js
+++ src/Algorithms/SortingAlgorithms.js
@@ -1,10 +1,10 @@
 export function getSortAnimations(arr, algorithm) {
     const animations = [];
 
-    if (arr.length <= 1) return arr;
-    
-    if (algorithm === "Selection") selectionSortHelper(arr.slice(), animations);
-    if (algorithm === "Insertion") insertionSortHelper(arr.slice(), animations);
+    if (algorithm === "Selection") selectionSortHelper(arr, animations);
+    else if (algorithm === "Insertion") insertionSortHelper(arr, animations);
+    else if (algorithm === "Bubble") bubbleSortHelper(arr, animations);
+    else if (algorithm === "Merge") mergeSortHelper(arr, animations, 0, arr.length - 1);
 
     return animations;
 }
@@ -30,23 +30,74 @@ function selectionSortHelper(copy, animations) {
     }
 }
 
-function swap(arr, i, j) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
-
 function insertionSortHelper(copy, animations) {
     const n = copy.length;
     animations.push([0, copy[0], 1])
 
     for (let i = 1; i < n; i++) {
-        for (let j = i; j > 0 && copy[j] < copy[j - 1]; j--) {
+        for (let j = i; j > 0; j--) {
             animations.push([j, j - 1, 0]);
             animations.push([j - 1, j, 0]);
+            if (copy[j] > copy[j - 1]) break;
             swap(copy, j, j - 1);
             animations.push([j, copy[j], 1]);
             animations.push([j - 1, copy[j - 1], 1]);
         }
     }
+}
+
+function bubbleSortHelper(copy, animations) {
+    const n = copy.length;
+    let sorted = false;
+
+    for (let end = n - 1; !sorted; end--) {
+        sorted = true;
+
+        for (let i = 0; i < end; i++) {
+            animations.push([i, i + 1, 0]);
+            animations.push([i + 1, i, 0]);
+            animations.push([i, i + 1, -1]);
+            animations.push([i + 1, i, -1]);
+            if (copy[i] > copy[i + 1]) {
+                swap(copy, i, i + 1);
+                animations.push([i, copy[i], 2]);
+                animations.push([i + 1, copy[i + 1], 2]);
+                sorted = false;
+            }
+        }
+
+        animations.push([end, copy[end], 1]);
+    }
+}
+
+function mergeSortHelper(copy, animations, left, right) {
+    if (left < right) {
+        let mid = left + (right - left) / 2;
+        mergeSortHelper(copy, animations, left, mid);
+        mergeSortHelper(copy, animations, mid + 1, right);
+        merge(copy, animations, left, mid, right);
+    }
+}
+
+function merge(copy, animations, left, mid, right) {
+    let n1 = mid - left + 1, n2 = right - mid;
+
+    let i = 0, j = 0;
+    while (i < n1 && j < n2) {
+        animations.push([left + i, mid + j, 0]);
+        animations.push([mid + j, left + i, 0]);
+        animations.push([left + i, mid + j, 1]);
+        animations.push([mid + j, left + i, 1]);
+        if (copy[left + i] < copy[mid + j]) i++;
+        else {
+            swap(copy, left + i, mid + j);
+            j++;
+        }
+    }
+}
+
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
--- src/Visualizer/Visualizer.jsx
+++ src/Visualizer/Visualizer.jsx
@@ -3,9 +3,9 @@ import { getSortAnimations } from '../Algorithms/SortingAlgorithms.js';
 import './Visualizer.css';
 
 const SPEED = 75;
-const BARS = 200;
-const PRIMARY_COLOR = 'black';
-const SECONDARY_COLOR = 'crimson';
+const BARS = 6;
+const UNSORTED = 'black';
+const COMPARE = 'crimson';
 const SORTED = 'chartreuse';
 const MOVED = 'darkviolet';
 
@@ -15,28 +15,43 @@ export default class Visualizer extends React.Component {
         this.state = {
             array: [],
         };
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
         this.makeArray();
     }
 
+    handleChange(event) {
+        this.makeArray(Math.floor((parseInt(event.target.value) + 3) * 1.65));
+    }
+
     makeArray() {
         const array = [];
-        const idx = randomInt(0, BARS);
         for (let i = 0; i < BARS; i++) {
-            if (i === idx) array.push(750);
-            else array.push(randomInt(5, 750));
+            array.push(randomInt(5, 750));
         }
+        array.push(750);
         this.setState( { array } );
     }
 
     markAsSorted(animations, bars) {
         for (let i = 0; i < animations.length; i++) {
             setTimeout(() => {
-                const b1style = bars[i].style;
-                b1style.backgroundColor = SORTED;
+                const [idx] = animations[i];
+                const bstyle = bars[idx].style;
+                if (bstyle.backgroundColor !== SORTED) bstyle.backgroundColor = SORTED;
             }, animations.length * SPEED);
+        }
+    }
+
+    reset(animations, bars) {
+        for (let i = 0; i < animations.length; i++) {
+            setTimeout(() => {
+                const [idx] = animations[i];
+                const bstyle = bars[idx].style;
+                bstyle.backgroundColor = UNSORTED;
+            }, animations.length * SPEED * 1.4);
         }
     }
 
@@ -51,7 +66,7 @@ export default class Visualizer extends React.Component {
                 const [b1idx, b2idx, c] = animations[i];
                 const b1style = bars[b1idx].style;
                 const b2style = bars[b2idx].style;
-                const color = c === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                const color = c === 0 ? COMPARE : UNSORTED;
                 setTimeout(() => {
                     b1style.backgroundColor = color;
                     b2style.backgroundColor = color;
@@ -67,6 +82,7 @@ export default class Visualizer extends React.Component {
         }
 
         this.markAsSorted(animations, bars);
+        this.reset(animations, bars);
     }
 
     visualizeInsertionSort() {
@@ -80,7 +96,7 @@ export default class Visualizer extends React.Component {
                 const [b1idx, b2idx, c] = animations[i];
                 const b1style = bars[b1idx].style;
                 const b2style = bars[b2idx].style;
-                const color = c === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                const color = c === 0 ? COMPARE : UNSORTED;
                 setTimeout(() => {
                     b1style.backgroundColor = color;
                     b2style.backgroundColor = color;
@@ -96,22 +112,72 @@ export default class Visualizer extends React.Component {
         }
 
         this.markAsSorted(animations, bars);
+        this.reset(animations, bars);
+    }
+
+    visualizeBubbleSort() {
+        const animations = getSortAnimations(this.state.array, "Bubble");
+        const bars = document.getElementsByClassName('array-bar');
+
+        for (let i = 0; i < animations.length; i++) {
+            const compare = animations[i][2] <= 0;
+
+            if (compare) {
+                const [b1idx, b2idx, c] = animations[i];
+                const b1style = bars[b1idx].style;
+                const b2style = bars[b2idx].style;
+                const color = c === 0 ? COMPARE : UNSORTED;
+                setTimeout(() => {
+                    b1style.backgroundColor = color;
+                    b2style.backgroundColor = color;
+                }, i * SPEED);
+            } else {
+                setTimeout(() => {
+                    const [b1idx, height, c] = animations[i];
+                    const b1style = bars[b1idx].style;
+                    b1style.height = `${height}px`;
+                    if (c === 1) b1style.backgroundColor = MOVED;
+                }, i * SPEED);
+            }
+        }
+
+        this.markAsSorted(animations, bars);
+        this.reset(animations, bars);
+    }
+
+    visualizeMergeSort() {
+        const animations = getSortAnimations(this.state.array, "Merge");
+        const bars = document.getElementsByClassNameByClassName('array-bar');
+
+        this.markAsSorted(animations, bars);
     }
 
     render() {
         const { array, isRunning } = this.state;
+        const numHeight = Math.floor(getHeight().height / 10);
+        const numWidth = Math.floor(getWidth().width / (array.length * 2));
+        const numMargin = array.length < 5 ?
+            10 : array.length < 8 ?
+            8 : array.length < 11 ?
+            6 : array.length < 20 ?
+            4 : array.length < 50 ?
+            3.5 : array.length < 100 ?
+            3 : array.length < 130 ?
+            2.5 : 2;
 
         return (
             <div id="page">
-                <div className="menu">
+                <div className="menu" style={{height: `${numHeight}px`}}>
                     <div className="button" onClick={!isRunning ? () => this.makeArray() : null}>Make New Array</div>
                 </div>
                 <div id="array-container">
-                    {array.map((value, idx) => ( <div className="array-bar" key = {idx} style={{ backgroundColor: PRIMARY_COLOR, height: `${value}px`, }} /> ))}
+                    {array.map((value, idx) => ( <div className="array-bar" key={idx} style={{height: `${value}px`, width: `${numWidth}px`, marginLeft: `${numMargin}px`, marginRight: `${numMargin}px`, backgroundColor: UNSORTED}} /> ))}
                 </div>
-                <div className="menu">
+                <div className="menu" style={{height: `${numHeight}px`}}>
                     <div className="button" onClick={!isRunning ? () => this.visualizeSelectionSort() : null}>Selection Sort</div>
                     <div className="button" onClick={!isRunning ? () => this.visualizeInsertionSort() : null}>Insertion Sort</div>
+                    <div className="button" onClick={!isRunning ? () => this.visualizeBubbleSort() : null}>Bubble Sort</div>
+                    <div className="button" onClick={!isRunning ? () => this.visualizeMergeSort() : null}>Merge Sort</div>
                 </div>
             </div>
         );
@@ -120,4 +186,14 @@ export default class Visualizer extends React.Component {
 
 function randomInt(a, b) {
     return Math.floor(Math.random() * (b - a + 1) + a);
+}
+
+function getWidth() {
+    const { innerWidth: width } = window;
+    return { width };
+}
+
+function getHeight() {
+    const { innerHeight: height } = window;
+    return { height };
 }
